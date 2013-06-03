996c55362c258409c77d97839c4e4d09f92d552a
Michael Mulich <michael.mulich@gmail.com>
2013-06-02 21:02:18 -0400

    This style wasn't working for me. Conflicted with other functionality.

--- personal/style.el
+++ personal/style.el
@@ -1,5 +1,5 @@
 ; Use the dichromacy theme
-(load-theme 'dichromacy t)
+;(load-theme 'dichromacy t)
 
 ; Disable whitespace, because the dichromacy theme doesn't work well with it.
-(add-hook 'prog-mode-hook 'prelude-turn-off-whitespace t)
+;(add-hook 'prog-mode-hook 'prelude-turn-off-whitespace t)
